0c8b860e7fefac856fd91917a68e92299d33fabb
Tianyao Ma <tianyao.ma333@gmail.com>
2021-12-14 22:18:29 -0500

    environment setup

---
+++ src/main/java/com/mty/hometogo/HomeToGoApplication.java
@@ -0,0 +1,13 @@
+package com.mty.hometogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeToGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeToGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mty/hometogo/HomeToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mty.hometogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
