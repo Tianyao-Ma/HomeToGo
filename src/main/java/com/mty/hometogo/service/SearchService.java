edf6659b50b439be7f7294fa72a500c43b8930c8
Tianyao Ma <tianyao.ma333@gmail.com>
2022-01-08 11:28:43 -0500

    add search service and controller

--- src/main/java/com/mty/hometogo/service/SearchService.java
+++ src/main/java/com/mty/hometogo/service/SearchService.java
@@ -1,4 +1,39 @@
 package com.mty.hometogo.service;
 
+import com.google.auto.value.AutoAnnotation;
+import com.mty.hometogo.model.Stay;
+import com.mty.hometogo.repository.LocationRepository;
+import com.mty.hometogo.repository.StayAvailabilityRepository;
+import com.mty.hometogo.repository.StayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class SearchService {
+    private StayRepository stayRepository;
+    private StayAvailabilityRepository stayAvailabilityRepository;
+    private LocationRepository locationRepository;
+
+    @Autowired
+    public SearchService(StayRepository stayRepository, StayAvailabilityRepository stayAvailabilityRepository, LocationRepository locationRepository) {
+        this.stayRepository = stayRepository;
+        this.stayAvailabilityRepository = stayAvailabilityRepository;
+        this.locationRepository = locationRepository;
+    }
+
+    public List<Stay> search(int guestNumber, LocalDate checkinDate, LocalDate checkoutDate, double lat, double lon, String distance) {
+        List<Long> stayIds = locationRepository.searchByDistance(lat, lon, distance);
+        long duration = Duration.between(checkinDate.atStartOfDay(), checkoutDate.atStartOfDay()).toDays();
+
+        List<Long> filteredStayIds = stayAvailabilityRepository.findByDateBetweenAndStateIsAvailable(stayIds, checkinDate, checkoutDate.minusDays(1), duration);
+        return stayRepository.findByIdInAndGuestNumberGreaterThanEqual(filteredStayIds, guestNumber);
+    }
+
+
+
+
 }
